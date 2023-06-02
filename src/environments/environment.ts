fe2395d6cf0470f34779cf962095f79fdf14a5b3
FeMott4 <125033689+FeMott4@users.noreply.github.com>
2023-06-02 13:37:12 +0000

    Tela de login e registro

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   firebaseConfig: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyAaSLnqwAH54W04mDdmresa5LEJONEBu6c",
+    authDomain: "aula1-02-06.firebaseapp.com",
+    projectId: "aula1-02-06",
+    storageBucket: "aula1-02-06.appspot.com",
+    messagingSenderId: "916252353588",
+    appId: "1:916252353588:web:997296184e68058f62d661",
+    measurementId: "G-7X4RMEYD6S"
   },
   production: false
 };
